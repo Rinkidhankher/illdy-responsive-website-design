46ee0564ae8de9ca0326d366d6644417d535c4e9
Rinkidhankher <rinki.dhankher@gmail.com>
2022-08-15 16:27:00 -0400

    first-commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function displaymenu() {
+    document.getElementById("closebutton").style.display = "block";
+    document.getElementById("menubutton").style.display = "none";
+    document.getElementById("mobile-options").style.display = "block";
+
+}
+
+function closemenu() {
+    document.getElementById("closebutton").style.display = "none";
+    document.getElementById("mobile-options").style.display = "none";
+    document.getElementById("menubutton").style.display = "block";
+}
